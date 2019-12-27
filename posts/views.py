c022342b60e41fd690375e749beaa1d328903aec
Snows02 <nieves.ardila02@gmail.com>
2019-12-26 23:26:50 -0500

    Mi primer template

--- posts/views.py
+++ posts/views.py
@@ -1,40 +1,42 @@
 """Posts views."""
 
 # Django
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # utilities
 from datetime import datetime
 
 POSTS = [
-    dict(
-        name='Mont Blac',
-        user='Yesica Cortes',
-        timestamp=datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        picture='https://picsum.photos/200/200/?image=1036',
-    ),
-    dict(
-        name='Via Láctea',
-        user='C. Vander',
-        timestamp=datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        picture='https://picsum.photos/200/200/?image=903',
-    ),
-    dict(
-        name='Nuevo auditorio',
-        user='Thespianartist',
-        timestamp=datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        picture='https://picsum.photos/200/200/?image=1076',
-    )
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 
 def list_posts(request):
     """List existing post."""
-    content = []
-    for post in POSTS:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i> {timestamp}</i></small></p>
-            <figure><img src="{picture}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': POSTS})
