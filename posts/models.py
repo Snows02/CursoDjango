4ac6bd24f6257fcfd1dec4ef0c91ffc864e92cd2
Snows02 <nieves.ardila02@gmail.com>
2019-12-27 21:47:21 -0500

    Creando mi primer modelo

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,31 @@
+"""Posts models."""
+
+# Django
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """Model definition for User."""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for User."""
+
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        """Unicode representation of User."""
+        pass
